de2a94b949ebb135d2fdf5826a346b75f656e92a
imratnesh <ratnesh.kush@yahoo.in>
2018-07-12 21:42:52 +0530

    Added django crisp forms.
    Signed-off-by: imratnesh <ratnesh.kush@yahoo.in>

--- rat-django-resume/settings.py
+++ rat-django-resume/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_rat.apps.DjangoRatConfig',
+    'django.forms',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
